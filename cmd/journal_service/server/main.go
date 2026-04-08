17308e28a72c2ab081ecf06e0b0ef17ebde9b728
Elias <eliaseromanov@gmail.com>
2026-04-08 18:07:54 +0700

    minor update

--- cmd/journal_service/server/main.go
+++ cmd/journal_service/server/main.go
@@ -3,16 +3,25 @@ package main
 import (
 	"github.com/EliasBlind/EduFlow/internal/journal_service/config"
 	"github.com/EliasBlind/EduFlow/internal/journal_service/logger"
+	"github.com/EliasBlind/EduFlow/internal/journal_service/validator"
+	"github.com/EliasBlind/EduFlow/pkg/interceptors"
+	"google.golang.org/grpc"
 )
 
 func main() {
 	cfg := config.MustLoad()
+	val := validator.New()
 
 	log := logger.MustLoad(config.EnvDev)
 
 	log.Info("start application")
-	
+
 	// TODO: init app
 
-	// TODO: started gRPC server
+	gRPCServer := grpc.NewServer(
+		grpc.UnaryInterceptor(interceptors.UnaryServerInterceptor(log)),
+	)
+	_ = cfg
+	_ = val
+	_ = gRPCServer
 }
--- go.mod
+++ go.mod
@@ -20,7 +20,6 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.5.0 // indirect
 	golang.org/x/crypto v0.47.0 // indirect
 	golang.org/x/net v0.49.0 // indirect
 	golang.org/x/sys v0.40.0 // indirect
